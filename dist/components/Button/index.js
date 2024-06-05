440cd30e9467e4eaf7b27d3086b281629229a72f
Devesh <devesh.agarwal@pw.live>
2024-06-05 14:10:09 +0530

    feat(PRG-0000): Init commit

--- dist/components/Button/index.js
+++ dist/components/Button/index.js
@@ -1,10 +1,10 @@
 import { jsx as s } from "react/jsx-runtime";
-import '../../assets/index3.css';const u = "_button_1m5ht_1", r = {
+import '../../assets/index3.css';const u = "_button_uc8j1_1", c = {
   button: u
 };
 function e(t) {
   const { className: o, ...n } = t;
-  return /* @__PURE__ */ s("button", { className: `${o} ${r.button}`, ...n });
+  return /* @__PURE__ */ s("button", { className: `${o} ${c.button}`, ...n });
 }
 export {
   e as Button
