cd51abb7934e99581ef8420ab758ef733be0d957
Devesh <devesh.agarwal@pw.live>
2024-06-05 14:27:32 +0530

    feat(PRG-0000): Clsx and tailwind-merge addition

--- dist/components/Button/index.js
+++ dist/components/Button/index.js
@@ -1,11 +1,12 @@
 import { jsx as s } from "react/jsx-runtime";
+import { cn as r } from "../../utils.js";
 import '../../assets/index3.css';const u = "_button_uc8j1_1", c = {
   button: u
 };
-function e(t) {
+function b(t) {
   const { className: o, ...n } = t;
-  return /* @__PURE__ */ s("button", { className: `${o} ${c.button}`, ...n });
+  return /* @__PURE__ */ s("button", { className: r(o, c.button), ...n });
 }
 export {
-  e as Button
+  b as Button
 };
